3b75f5c9c945190a1a7c8e9c366ba288415cc9d8
cgarcia <cgarcia@compuhora.com.co>
2020-08-06 19:13:50 -0500

    Initial Structure

---
+++ Core/Reflection.js
@@ -0,0 +1,71 @@
+module.exports = (function(){
+
+    /**
+     * 
+     * @param {*} entity Entilite Entity Object
+     */
+    let getColumns = function(entity){}
+
+
+    /**
+     * 
+     * @param {*} entity Entilite Entity Object
+     */
+    let getPrimaryKey = function(entity){}
+
+
+    /**
+     * 
+     * @param {*} entity Entilite Entity Object
+     */
+    let getForeingKeys = function(entity){}
+
+
+    /**
+     * 
+     * @param {*} entity Entilite Entity Object
+     */
+    let getColumnsWithForeingKeys = function(entity){}
+
+
+    /**
+     * 
+     * @param {*} entity 
+     * Error Returns:
+     * 
+     */
+    let validEntity = function(entity){}
+
+    /**
+     * 
+     * @param {*} entityData Entilite Entity Data Object
+     */
+    let validData = function(entityData){}
+
+
+    let validRelationalOperator = function(){}
+
+    let validLogicalOperator = function(){}
+
+    let validColumnsData = function(){}
+
+    let isClientValid = function(){}
+
+    let isContextValid = function(){}
+
+
+
+    let getJoinQuery = function(entity){}
+
+    let getSelectQuery = function(entity){}
+
+    let getInsertQuery = function(entity){}
+
+    let getDeleteQuery = function(entity){}
+
+    let getUpdateQuery = function(entity){}
+
+    return {
+
+    }
+});
---
+++ Core/Wrapper.js
@@ -0,0 +1,16 @@
+module.exports = (function(){
+
+    let query = function(contextClient, query, params){}
+
+    let select = function(contextClient, entity, columns, conditions, orderby){}
+
+    let update = function(contextClient, entity, data, conditions){}
+
+    let del = function(contextClient, entity, conditions){}
+
+    let insert = function(contextClient, entity, data){}
+    
+    return {
+
+    }
+});
